7d87abbd77217be5501ef7e441ddc62dbfc42454
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-08-23 23:45:34 -0300

    feat: get temperature of esp32 thermistor websocket.

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := gin.Default()
+
+	router.GET("/ws", func(ctx *gin.Context) {
+		conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+		if err != nil {
+			return
+		}
+
+		defer conn.Close()
+
+		for {
+			_, temperature, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Println("Error to read temperature!")
+				break
+			}
+
+			fmt.Println("Actual temperature: ", string(temperature))
+		}
+	})
+
+	router.Run()
 }
